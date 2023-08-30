055ba358cee0918a49bc68e046fd30d7d4b03f66
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-29 22:49:46 -0300

    Create model_training.py

---
+++ model_training.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
