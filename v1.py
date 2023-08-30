b78f8f924515f32030632883845410a62225e848
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-29 22:47:11 -0300

    Create v1.py
    Changes and debugging applied:
    
        Added a print statement for the classification report to provide detailed evaluation metrics.
        Ensured that the vectorizer object is used consistently for transforming both training and testing data.
        Provided a more informative output message for the prediction result.
    
    Remember to replace 'historical_event_data.csv' with the actual path to your historical event data CSV file. Additionally, make sure that you have the necessary libraries (pandas, scikit-learn) installed in your Python environment.
    
    Keep in mind that this script still provides a basic implementation. For real-world use, you might want to explore more advanced techniques, hyperparameter tuning, cross-validation, and handling potential class imbalances in the dataset.

---
+++ v1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+# Load historical event data (Assuming CSV format with 'event_description' and 'is_privilege_escalation' columns)
+data = pd.read_csv('historical_event_data.csv')
+
+# Data preprocessing
+X = data['event_description']
+y = data['is_privilege_escalation']
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Feature extraction using TF-IDF
+vectorizer = TfidfVectorizer(max_features=1000, stop_words='english')
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+# Train a Random Forest classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train_tfidf, y_train)
+
+# Evaluate the model
+y_pred = model.predict(X_test_tfidf)
+print("Classification Report:\n", classification_report(y_test, y_pred))
+
+# Now let's assume you have an incoming event description
+incoming_event = "User JohnDoe attempted to access restricted files."
+
+# Preprocess the incoming event description
+incoming_event_tfidf = vectorizer.transform([incoming_event])
+
+# Use the trained model to predict if it's a privilege escalation event
+prediction = model.predict(incoming_event_tfidf)
+
+if prediction[0] == 1:
+    print("Suspicious: Potential privilege escalation detected.")
+else:
+    print("Normal event.")
